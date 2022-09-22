28db1f18cf50618e83c7180c30c2ad38e951e8a0
Rishvanth K S <83532062+iamRishvanth@users.noreply.github.com>
2022-09-22 13:04:17 +0530

    Update tracker.js

--- tracker.js
+++ tracker.js
@@ -1,1 +1,36 @@
-console.log(Testing);
+const user = document.getElementById("username");
+const pass = document.getElementById("password");
+const gallery = document.getElementById("gallery");
+
+const fetchDetails = () => {
+
+    const array_res = [];
+    const url = `https://randomuser.me/api/?results=10`;
+    fetch(url).then(res => res.json()).then(data => console.log(data.results[0].name.first));
+    //const details = {
+    //id: array_res[0].gender,
+    //pass: array_res[0].login.password,
+    //name: array_res[0].login.username,
+    //email: array_res[0].email,+
+    //location: array_res[0].location.country,
+    //image: array_res[0].picture.medium,
+    //}
+    //console.log(details);
+    //displayDetails(details);
+};
+
+const displayDetails = (details) => {
+    console.log(details);
+    const detailsHTMLString = details.map(info => `
+        <li class = "card">
+            <img class="card_image" src="${info.image}"/>
+            <h3 class="card_name"> ${info.name}</h2>
+            <p class="card_mail"> ${info.mail}</p>
+            <p class="card_location"> ${info.location}</p>
+            <p class="card_amount">100000</p>
+        </li>
+    `)
+    gallery.innerHTML = detailsHTMLString;
+};
+
+fetchDetails();
